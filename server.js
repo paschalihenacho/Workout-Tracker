982f90c03dc1904d825a63606530b1dbb0292e2d
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-08-08 00:43:41 -0500

    get rountes

--- server.js
+++ server.js
@@ -2,7 +2,38 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const Workout = require('./models/workout');
+let mongoose = require('mongoose');
+let db = require('./models');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(morgan('tiny'));
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
+
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/stats.html'));
+});
+
+app.get('/api/workouts', async (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
