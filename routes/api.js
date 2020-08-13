d3bfd437ce007c0ed45454bc4e061074d1b500b3
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-08-12 23:04:15 -0500

    adding exercises with daily workouts

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,43 @@ const app = require("express").Router();
 const Workout = require("../models/workouts.js");
 
 app.post("/api/workouts", ({ body }, res) => {
-  console.log("Creating a new workout")
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+	Workout.create(body)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
 });
+app.put("/api/workouts/:id", (req, res) => {
+	Workout.findByIdAndUpdate(
+		req.params.id,
+		{ $push: { exercises: req.body } },
+		{ new: true }
+	)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+app.get("/api/workouts", (req, res) => {
+	Workout.find({})
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+app.get("/api/workouts/range", (req, res) => {
+	Workout.find({})
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+module.exports = app;
