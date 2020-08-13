6bb5e2878b074ae20113fbbe0b8dabd9ac3d67a7
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-08-12 22:39:30 -0500

    fixed routings

--- server.js
+++ server.js
@@ -1,89 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const Workout = require('./models/workout');
-let mongoose = require('mongoose');
-let db = require('./models');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan"); 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI,{
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-app.use(morgan('tiny'));
-
-app.use(express.static('public'));
-
+app.use(morgan("dev"));
+app.use(express.static("public")); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
-
-app.get('/exercise', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/exercise.html'));
-});
-
-app.get('/stats', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/stats.html'));
-});
-
-app.get('/api/workouts', async (req, res) => {
-  Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.post('/api/workouts', async (req, res) => {
-    const workout = new Workout({ exercises: req.body });
-    Workout.create(workout)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  app.put('/api/workouts/:id', async (req, res) => {
-    const id = req.params.id;
-    const data = req.body;
-  
-    Workout.findById(id)
-      .then(dbWorkout => {
-        dbWorkout.exercises.push(data);
-        return dbWorkout;
-      })
-      .then(dbWorkout => {
-        Workout.findOneAndUpdate(
-          { _id: id },
-          { exercises: dbWorkout.exercises },
-          { new: true }
-        )
-          .then(dbWorkout => {
-            res.json(dbWorkout);
-          })
-          .catch(err => {
-            res.json(err);
-          });
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  app.get('/api/workouts/range', async (req, res) => {
-    Workout.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  app.listen(PORT, function() {
-    console.log('App now listening at localhost:' + PORT);
-  });
